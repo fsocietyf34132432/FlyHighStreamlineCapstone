2de0b56cfb0a1d73b1f77d75fd2dc78990e60fbe
plyngod <plyngod@DESKTOP-NR8C6EJ>
2024-09-02 20:12:32 +0800

    new

--- FlyHighStreamlineCapstone/ViewModel/FlightViewModel.cs
+++ FlyHighStreamlineCapstone/ViewModel/FlightViewModel.cs
@@ -14,9 +14,14 @@ namespace FlyHighStreamlineCapstone.ViewModel
 
     
         public int Duration { get; set; }
+        [Display(Name = "Departure Airport Name")]
         public int DepartureAirportId { get; set; }
+        [Display(Name = "Arrival Airport Name")]
         public int ArrivalAirportId { get; set; }
+
+        [Display(Name = "Arline Name")]
         public int AirlineId { get; set; }
+        [Display(Name = "Aircraft")]
         public int AircraftId { get; set; }
         public int AirportId { get; set; }
     }
